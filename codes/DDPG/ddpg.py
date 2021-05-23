025ca65aa0e5652c32a1918725458168a80040f3
liuminwen <84269882+liuminwen@users.noreply.github.com>
2021-05-23 20:49:18 +0800

    Add files via upload

---
+++ codes/DDPG/ddpg.py
@@ -0,0 +1,265 @@
+import os, sys, random
+from itertools import count 
+import numpy as np
+import gym
+import time
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions import Normal
+from tensorboardX import SummaryWriter
+
+TAU = 0.005
+LR = 1e-3
+GAMMA = 0.99
+MEMORY_CAPACITY = 5000
+BATCH_SIZE = 64
+MAX_EPISODE = 20000
+MODE = 'test'#'train' # or 'test'
+
+sample_frequency = 256
+log_interval = 50
+render_interval = 100
+exploration_noise = 0.1
+max_length_of_trajectory = 2000
+target_update_interval = 1
+test_iteration = 10
+update_iteration = 10
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+env = gym.make('Pendulum-v0').unwrapped
+state_dim = env.observation_space.shape[0]
+action_dim = env.action_space.shape[0]
+max_action = float(env.action_space.high[0])
+min_Val = torch.tensor(1e-7).float().to(device)
+
+directory = './runs'
+
+########
+class Replay_buffer():
+    def __init__(self,max_size=MEMORY_CAPACITY):
+        self.storage = []
+        self.max_size = max_size
+        self.ptr = 0
+
+    def push(self,data):
+        if len(self.storage) == self.max_size:
+            self.storage[int(self.ptr)] = data
+            self.ptr = (self.ptr+1) % self.max_size
+        else:
+            self.storage.append(data)
+
+    def sample(self,batch_size):
+        ind = np.random.randint(0,len(self.storage),size=batch_size)
+        x,y,u,r,d = [],[],[],[],[]
+
+        for i in ind:
+            X,Y,U,R,D = self.storage[i]
+            x.append(np.array(X,copy=False))
+            y.append(np.array(Y,copy=False))
+            u.append(np.array(U,copy=False))
+            r.append(np.array(R,copy=False))
+            d.append(np.array(D,copy=False))
+        return np.array(x),np.array(y),np.array(u),np.array(r),np.array(d)
+
+
+############
+class Actor(nn.Module):
+    """docstring for Actor"""
+    def __init__(self, state_dim,action_dim,max_action):
+        super(Actor, self).__init__()
+        
+        self.l1 = nn.Linear(state_dim,400)
+        self.l2 = nn.Linear(400,300)
+        self.l3 = nn.Linear(300,action_dim)
+        self.max_action = max_action
+
+    def forward(self,x):
+        x = F.relu(self.l1(x))
+        x = F.relu(self.l2(x))
+        x = self.max_action * torch.tanh(self.l3(x))
+        return x
+
+class Critic(nn.Module):
+    """docstring for Critic"""
+    def __init__(self, state_dim,action_dim):
+        super(Critic, self).__init__()
+        self.l1 = nn.Linear(state_dim+action_dim,400)
+        self.l2 = nn.Linear(400,300)
+        self.l3 = nn.Linear(300,1)
+        
+    def forward(self,x,u):
+        x = F.relu(self.l1(torch.cat([x,u],1)))
+        x = F.relu(self.l2(x))
+        x = self.l3(x)
+        return x
+
+###########
+class DDPG(object):
+    """docstring for DDPG"""
+    def __init__(self, state_dim,action_dim,max_action):
+        super(DDPG, self).__init__()
+        
+        self.actor = Actor(state_dim,action_dim,max_action).to(device)
+        self.actor_target = Actor(state_dim,action_dim,max_action).to(device)
+        self.actor_target.load_state_dict(self.actor.state_dict())
+        self.actor_optimizer = optim.Adam(self.actor.parameters(),LR)
+
+        self.critic = Critic(state_dim,action_dim).to(device)
+        self.critic_target = Critic(state_dim,action_dim).to(device)
+        self.critic_target.load_state_dict(self.critic.state_dict())
+        self.critic_optimizer = optim.Adam(self.critic.parameters(),LR)
+
+        self.replay_buffer = Replay_buffer()
+        self.writer = SummaryWriter(directory)
+        self.num_critic_update_iteration = 0
+        self.num_actor_update_iteration = 0
+        self.num_training = 0
+
+    def select_action(self,state):
+        state = torch.FloatTensor(state.reshape(1,-1)).to(device)
+        return self.actor(state).cpu().data.numpy().flatten()
+
+    def update(self):
+        for it in range(update_iteration):
+            # sample replay buffer
+            x,y,u,r,d = self.replay_buffer.sample(BATCH_SIZE)
+            state = torch.FloatTensor(x).to(device)
+            action = torch.FloatTensor(u).to(device)
+            next_state = torch.FloatTensor(y).to(device)
+            done = torch.FloatTensor(d).to(device)
+            reward = torch.FloatTensor(r).to(device)
+
+            # compute the target Q value
+            target_Q = self.critic_target(next_state,self.actor_target(next_state))
+            #target_Q = reward + ((1-done)*GAMMA*target_Q).detach()
+            target_Q = reward.reshape([64,1]) + ((((1-done)*GAMMA).reshape([64,1]))*target_Q).detach()
+            
+            # get current Q estimate
+            current_Q = self.critic(state,action)
+
+            # compute critic loss
+            critic_loss = F.mse_loss(current_Q,target_Q)
+            self.writer.add_scalar('Loss/critic_loss',critic_loss,global_step=self.num_critic_update_iteration)
+
+            # optimize the critic
+            self.critic_optimizer.zero_grad()
+            critic_loss.backward()
+            self.critic_optimizer.step()
+
+            # compute actor loss
+            actor_loss = - self.critic(state,self.actor(state)).mean()
+            self.writer.add_scalar('Loss/actor_loss',actor_loss,global_step=self.num_actor_update_iteration)
+
+            # optimize the actor
+            self.actor_optimizer.zero_grad()
+            actor_loss.backward()
+            self.actor_optimizer.step()
+
+            # update the frozen target models
+            for param,target_param in zip(self.critic.parameters(),self.critic_target.parameters()):
+                target_param.data.copy_(TAU * param.data + (1-TAU) * target_param.data)
+
+            for param,target_param in zip(self.actor.parameters(),self.actor_target.parameters()):
+                target_param.data.copy_(TAU * param.data + (1-TAU) * target_param.data) 
+
+            self.num_actor_update_iteration += 1
+            self.num_critic_update_iteration += 1
+
+
+    def save(self):
+        torch.save(self.actor.state_dict(),directory+'actor.pth')
+        torch.save(self.critic.state_dict(),directory+'critic.pth')
+        print('model has been saved...')
+
+    def load(self):
+        self.actor.load_state_dict(torch.load(directory+'actor.pth'))
+        self.critic.load_state_dict(torch.load(directory+'critic.pth'))
+        print('model has been loaded...')
+
+
+#############
+def main():
+    agent = DDPG(state_dim,action_dim,max_action)
+    ep_r = 0
+
+    if MODE == 'test':
+        agent.load()
+        for i in range(test_iteration):
+            state = env.reset()
+            for t in count():
+                env.render()
+                action = agent.select_action(state)
+                next_state, reward, done, info = env.step(np.float32(action))
+                ep_r += reward
+                env.render()
+                if done or t>=100:
+                    print('Episode:{}, Return:{:0.2f}, Step:{}'.format(i,ep_r,t))
+                    ep_r = 0
+                    break
+                state = next_state
+                time.sleep(0.02)
+
+    elif MODE == 'train':
+        print('Collection Experience...')
+
+        for i in range(MAX_EPISODE):
+            state = env.reset()
+            for t in count():
+                #env.render()
+                #time.sleep(0.01)
+                action = agent.select_action(state)
+
+                # issue 3 add noise to action
+                action = (action + np.random.normal(0,exploration_noise,size=env.action_space.shape[0])).clip(env.action_space.low,env.action_space.high)
+
+                next_state, reward, done, info = env.step(action)
+                ep_r += reward
+                agent.replay_buffer.push((state,next_state,action,reward,np.float(done)))
+                
+                state = next_state
+                if done or t>=max_length_of_trajectory:
+                    agent.writer.add_scalar('ep_r',ep_r,global_step=i)
+                    if i % 10 ==0:
+                        print('Episode:{}, Return:{:0.2f}, Step:{}'.format(i,ep_r,t))
+                    ep_r = 0
+                    break
+           
+            if (i+1) % 100 == 0:
+                print('Episode:{}, Memory size:{}'.format(i,len(agent.replay_buffer.storage)))
+
+            if i % log_interval == 0:
+                agent.save()
+
+            if len(agent.replay_buffer.storage) >= MEMORY_CAPACITY-1:
+                agent.update()
+            
+
+    else:
+        raise NameError('model is wrong!!!')
+
+if __name__ == '__main__':
+    main()
+
+###保存模型后再导入接着训练得到的正确率却低的原因是：
+#epsilon = 0.5 * (0.99 ** episode)里面的episode有关，你重新开始的时候，episode从0开始，也就意味着ai在大胆地进行探索
+#注，代码里虽然没有epsilon但是实际上Adam里会用到
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ codes/DQN/dqn.py
@@ -0,0 +1,320 @@
+import numpy as np
+import pandas as pd
+import time
+import gym
+import csv
+import os
+import pickle
+from queue import Queue
+import pickle
+import random
+from itertools import count 
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions import Normal
+from tensorboardX import SummaryWriter
+
+
+max_length_of_trajectory = 500
+MEMORY_CAPACITY = 5000
+BATCH_SIZE = 64
+MAX_EPISODE = 20000
+MODE = 'train'#'train' # or 'test'
+sample_frequency = 256
+log_interval = 50
+test_iteration=10
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+directory = './runs'
+
+class Critic(nn.Module):
+    """docstring for Critic"""
+    def __init__(self,state_dim,action_dim):
+        super(Critic, self).__init__()
+        self.l1 = nn.Linear(state_dim,64)
+        self.l2 = nn.Linear(64,32)
+        self.l3 = nn.Linear(32,action_dim)
+        
+    def forward(self,x):
+        x = F.relu(self.l1(x))
+        x = F.relu(self.l2(x))
+        x = self.l3(x)
+        return x
+
+class Replay_buffer():
+    def __init__(self,max_size=MEMORY_CAPACITY):
+        self.storage = []
+        self.max_size = max_size
+        self.ptr = 0
+
+    def push(self,data):
+        if len(self.storage) == self.max_size:
+            self.storage[int(self.ptr)] = data
+            self.ptr = (self.ptr+1) % self.max_size
+        else:
+            self.storage.append(data)
+
+    def sample(self,batch_size):
+        ind = np.random.randint(0,len(self.storage),size=batch_size)
+        x,y,index,u,r,d = [],[],[],[],[],[]
+
+        for i in ind:
+            X,Y,I,U,R,D = self.storage[i]
+            x.append(np.array(X,copy=False))
+            y.append(np.array(Y,copy=False))
+            index.append(np.array(I,copy=False))
+            u.append(np.array(U,copy=False))
+            r.append(np.array(R,copy=False))
+            d.append(np.array(D,copy=False))
+        return np.array(x),np.array(y),np.array(index),np.array(u),np.array(r),np.array(d)
+
+
+class DQN:
+    def __init__(self,tau=0.005,state_dim=3,test='False',learning_rate=1e-2, reward_decay=0.99, e_greedy=1,epsilon_decay=0.995,epsilon_min=0.01,):
+        #self.target                     # 目标状态（终点）
+        self.test=test
+        self.lr = learning_rate         # 学习率
+        self.gamma = reward_decay       # 回报衰减率
+        self.epsilon = e_greedy         # 探索/利用 贪婪系数
+        self.epsilon_decay=epsilon_decay    
+        self.epsilon_min = epsilon_min     
+        self.tau=tau
+        self.num_cos = 10               #分为多少份
+        self.num_sin = 10               
+        self.num_dot = 10 
+        self.num_actions = 10 
+        self.actions = self.toBins(-2.0, 2.0, self.num_actions)    # 可以选择的动作空间  离散化
+        #########=============== 状态空间离散化，但实际上DQN的状态空间可以不离散，如果离散的话，取消注释digital、digitize_state相关的函数===========
+        # self.cos_bins = self.toBins(-1.0, 1.0, self.num_cos)
+        # self.sin_bins = self.toBins(-1.0, 1.0, self.num_sin)
+        # self.dot_bins = self.toBins(-8.0, 8.0, self.num_dot)
+        self.state_dim=state_dim
+        #
+        self.critic = Critic(self.state_dim,self.num_actions).to(device)
+        self.critic_target = Critic(self.state_dim,self.num_actions).to(device)
+        self.critic_target.load_state_dict(self.critic.state_dict())
+        self.critic_optimizer = optim.Adam(self.critic.parameters(),self.lr)
+
+        self.replay_buffer = Replay_buffer()
+        self.writer = SummaryWriter(directory)
+        self.num_critic_update_iteration = 0
+        self.num_training = 0
+        #
+
+
+
+    # 根据本次的行动及其反馈（下一个时间步的状态），返回下一次的最佳行动
+    
+
+    # 分箱处理函数，把[clip_min,clip_max]区间平均分为num段，  如[1,10]分为5.5 
+    def toBins(self,clip_min, clip_max, num):
+        return np.linspace(clip_min, clip_max, num + 1)[1:-1] #第一项到倒数第一项
+
+    # 分别对各个连续特征值进行离散化  如[1,10]分为5.5  小于5.5取0  大于取5.5取1
+    def digit(self,x, bin): 
+        n = np.digitize(x,bins = bin)
+        return n
+
+    # 将观测值observation离散化处理
+    def digitize_state(self,observation):
+        # 将矢量打散回3个连续特征值
+        cart_sin, cart_cos , cart_dot = observation
+        # 分别对各个连续特征值进行离散化（分箱处理）
+        digitized = [self.digit(cart_sin,self.cos_bins),
+                    self.digit(cart_cos,self.sin_bins),
+                    self.digit(cart_dot,self.dot_bins),]
+        # 将离散值再组合为一个离散值，作为最终结果
+        return (digitized[1]*self.num_cos + digitized[0]) * self.num_dot + digitized[2]
+
+    def act(self,state): 
+        self.epsilon *= self.epsilon_decay
+        self.epsilon = max(self.epsilon_min, self.epsilon)
+        epsilon = 0.01 if self.test else self.epsilon  # use epsilon = 0.01 when testing
+        state = torch.FloatTensor(state.reshape(1,-1)).to(device)
+        q_values = self.critic(state).cpu().data.numpy().flatten()
+        #self.summaries['q_val'] = max(q_values)
+        if np.random.random() < epsilon:
+            action_index = np.random.choice(self.num_actions)# sample random action
+        else:
+            action_index = np.argmax(q_values)
+        action = -2 + 4/(self.num_actions-1) * action_index  
+        return action_index,action
+
+
+    # update，主要是更新Q值
+    def update(self):
+        x,y,index,u,r,d = self.replay_buffer.sample(BATCH_SIZE)
+        state = torch.FloatTensor(x).to(device)
+        action = torch.FloatTensor(u).to(device)
+        next_state = torch.FloatTensor(y).to(device)
+        reward = torch.FloatTensor(r).to(device)
+        done = torch.FloatTensor(d).to(device)
+
+        # compute the target Q value
+# =============================================================================
+#DDQN
+#         target_Q = self.critic_target(next_state)
+#         #target_Q = reward + ((1-done)*GAMMA*target_Q).detach()
+#         target_Q = reward.reshape([64,1]) + ((((1-done)*self.gamma).reshape([64,1]))*target_Q).detach()
+# =============================================================================
+        target_Q = self.critic_target(next_state).max(axis=1)[0].reshape([64,1])
+        target_Q=reward.reshape([64,1]) + ((((1-done)*self.gamma).reshape([64,1]))*target_Q)
+        # get current Q estimate
+        #current_Q = self.critic(state).max(axis=1)[0].reshape([64,1])
+        current_Q = self.critic(state)[np.arange(64),index].reshape([64,1])
+
+        # compute critic loss
+        critic_loss = F.mse_loss(current_Q,target_Q)
+        self.writer.add_scalar('Loss',critic_loss,global_step=self.num_critic_update_iteration)
+
+        # optimize the critic
+        self.critic_optimizer.zero_grad()
+        critic_loss.backward()
+        self.critic_optimizer.step()
+
+# =============================================================================
+#DDQN
+#         # update the frozen target models
+#         for param,target_param in zip(self.critic.parameters(),self.critic_target.parameters()):
+#             target_param.data.copy_(self.tau * param.data + (1-self.tau) * target_param.data)
+# =============================================================================
+
+        self.num_critic_update_iteration += 1
+
+    def save(self):
+        torch.save(self.critic.state_dict(),directory+'critic.pth')
+        print('model has been saved...')
+
+    def load(self):
+        self.critic.load_state_dict(torch.load(directory+'critic.pth'))
+        print('model has been loaded...')
+
+def train():
+    ep_r=0
+    env = gym.make('Pendulum-v0')   
+    #print(env.action_space)
+    agent = DQN(learning_rate=1e-2)
+    # with open(os.getcwd()+'/tmp/Pendulum.model', 'rb') as f:
+    #     agent = pickle.load(f)
+    action = [0]    #输入格式要求 要是数组
+    for i in range(MAX_EPISODE):  #训练次数
+        state = env.reset()  #状态  cos(theta), sin(theta) , thetadot角速度
+        #state = agent.digitize_state(state)  #状态标准化
+        for t in count():   #一次训练最大运行次数
+            action_index,action[0] = agent.act(state)  #动作 -2到2
+            next_state, reward, done, info = env.step(action)   
+            #next_state = agent.digitize_state(next_state)
+            #下面这两个if语句可以加快训练
+            # if done:
+            #     reward-=200  #对于一些直接导致最终失败的错误行动，其报酬值要减200
+            if reward >= -0.5:  #竖直时时reward接近0  -10到0
+                reward+=20   #给大一点
+            #     #print('arrive')
+            # # print(action,reward,done,state,next_state)
+            ep_r+=reward
+            agent.replay_buffer.push((state,next_state,action_index,action[0],reward,float(done)))
+            #agent.learn(state,action[0],reward,next_state)
+            state = next_state
+            agent.update()
+            if done or t>=max_length_of_trajectory:
+                agent.writer.add_scalar('ep_r',ep_r,global_step=i)
+                if i % 10 ==0:
+                    print('Episode:{}, Return:{:0.2f}, Step:{}'.format(i,ep_r,t))
+                ep_r = 0
+                break
+            # env.render()    # 更新并渲染画面
+        if (i+1) % 100 == 0:
+                print('Episode:{}, Memory size:{}'.format(i,len(agent.replay_buffer.storage)))
+
+        if i % log_interval == 0:
+            agent.save()
+
+    
+def test():
+    ep_r=0
+    agent=DQN()
+    env = gym.make('Pendulum-v0')   
+    agent.load()
+    action=[0]
+    for i in range(test_iteration):
+        state = env.reset()  #状态  cos(theta), sin(theta) , thetadot角速度
+        #state = agent.digitize_state(state)  #状态标准化
+        for t in count():
+            action_index,action[0]  = agent.act(state)
+            next_state, reward, done, info = env.step(action)   
+            #next_state = agent.digitize_state(next_state)
+            ep_r += reward
+            env.render()
+            if done or t>=max_length_of_trajectory:
+                print('Episode:{}, Return:{:0.2f}, Step:{}'.format(i,ep_r,t))
+                ep_r = 0
+                break
+            state = next_state
+            time.sleep(0.05)
+
+def run_test():
+    env = gym.make('Pendulum-v0')   
+    action = [0]
+    observation = env.reset()  #状态   
+    # print(env.action_space)
+    # print(observation)
+    actions = np.linspace(-2, 2, 10)
+    for t in range(100):   #
+        # action[0] =  random.uniform(-2,2)   #力矩  -2到2 
+        action[0] = 2
+        observation, reward, done, info = env.step(action)   
+        #print(action,reward,done)
+       
+        # print('observation:',observation)
+        # print('theta:',env.state)
+        env.render() 
+        time.sleep(1)
+    env.close()
+
+if __name__ == '__main__':
+    train()
+    #test()
+    #run_test()
+    # ep_r=0
+    # env = gym.make('Pendulum-v0')   
+    # #print(env.action_space)
+    # agent = DQN(learning_rate=1e-2)
+    # # with open(os.getcwd()+'/tmp/Pendulum.model', 'rb') as f:
+    # #     agent = pickle.load(f)
+    # action = [0]    #输入格式要求 要是数组
+    # for i in range(MAX_EPISODE):  #训练次数
+    #     state = env.reset()  #状态  cos(theta), sin(theta) , thetadot角速度
+    #     #state = agent.digitize_state(state)  #状态标准化
+    #     for t in count():   #一次训练最大运行次数
+    #         action_index,action[0] = agent.act(state)  #动作 -2到2
+    #         next_state, reward, done, info = env.step(action)   
+    #         #next_state = agent.digitize_state(next_state)
+    #         #下面这两个if语句可以加快训练
+    #         if done:
+    #             reward-=200  #对于一些直接导致最终失败的错误行动，其报酬值要减200
+    #         if reward >= -0.5:  #竖直时时reward接近0  -10到0
+    #             reward+=40   #给大一点
+    #         #     #print('arrive')
+    #         # # print(action,reward,done,state,next_state)
+    #         ep_r+=reward
+    #         agent.replay_buffer.push((state,next_state,action_index,action[0],reward,float(done)))
+    #         #agent.learn(state,action[0],reward,next_state)
+    #         state = next_state
+    #         agent.update()
+    #         if done or t>=max_length_of_trajectory:
+    #             agent.writer.add_scalar('ep_r',ep_r,global_step=i)
+    #             if i % 10 ==0:
+    #                 print('Episode:{}, Return:{:0.2f}, Step:{}'.format(i,ep_r,t))
+    #             ep_r = 0
+    #             break
+    #         # env.render()    # 更新并渲染画面
+    #     if (i+1) % 100 == 0:
+    #             print('Episode:{}, Memory size:{}'.format(i,len(agent.replay_buffer.storage)))
+
+    #     if i % log_interval == 0:
+    #         agent.save()
+
